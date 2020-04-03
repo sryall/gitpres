421037116206d9b021b85ed2cc27ef525d353c3d
armw37 <anderson.rm.white@gmail.com>
2020-04-03 10:00:08 -0400

    Add Files

---
+++ sis_unfinished.py
@@ -0,0 +1,13 @@
+# Example for the group
+
+import matplotlib.pyplot as plt
+import sys
+
+script_location = '/home/anderson/Documents/UW_AM_Research' + \
+        '/Code/GEMFPython'
+
+sys.path.append(script_location)
+
+from GEMFPy import *
+
+
